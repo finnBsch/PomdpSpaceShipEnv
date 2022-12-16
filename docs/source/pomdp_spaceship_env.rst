72cadd3e0e93466689e267ee7994e671656265d9
finnbsch <finn.lukas.busch@gmail.com>
2022-12-16 12:49:23 +0100

    Fixed Docs

--- docs/source/pomdp_spaceship_env.rst
+++ docs/source/pomdp_spaceship_env.rst
@@ -3,5 +3,6 @@ Pomdp Space Ship Environment
 
 .. automodule:: pomdp_spaceship_env
    :members:
+   :exclude-members: Viz, ResX, ResY, AutoReset, DynamicGoals, NumObs, PrintLevel, ShareEnvs, SizeX, SizeY
    :undoc-members:
 
--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from skbuild import setup
 
 setup(
     name="pomdp_spaceship_env",
-    version="1.2.3",
-    description="a minimal example package (with pybind11)",
-    author='Pablo Hernandez-Cerdan',
+    version="0.0.1,
+    description="A Partially Observable Space Ship Environment for RL",
+    author='Finn Lukas Busch',
+    author_email = 'finn.lukas.busch@gmail.com'
     license="MIT",
     packages=['pomdp_spaceship_env'],
     package_dir={'': 'src'},
     cmake_install_dir='src/pomdp_spaceship_env',
-    python_requires='>=3.7',
+    python_requires='>=3.9',
     include_package_data=True,
 )
